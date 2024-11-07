e26de1d3c7a9e75397f7965ae8bd1aed0decb14d
Diego Valencia <diegovalh08@gmail.com>
2024-11-07 18:42:37 -0500

    Conexion a DB

--- src/main/java/co/redcom/entity/Persona.java
+++ src/main/java/co/redcom/entity/Persona.java
@@ -17,7 +17,7 @@ public class Persona {
     private Integer idPersona;
 
     @Column(nullable = false)
-    @NotEmpty(message = "El nombre no puede ser nulo es obligatorio ")
+    @NotEmpty(message = "El nombre no puede ser nulo ")
     private String nombre;
 
     //-----------------> Auditoria <--------------------
